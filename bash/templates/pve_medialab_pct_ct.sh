13b0897ad342f3c45fab4bfc32327fcdfd2058a8
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2021-02-08 16:33:10 +0700

    Update pve_medialab_pct_ct.sh

--- bash/templates/pve_medialab_pct_ct.sh
+++ bash/templates/pve_medialab_pct_ct.sh
@@ -18,5 +18,5 @@ TEMPLATE_STRING="local:vztmpl/${TEMPLATE}"
 # Create LXC
 msg "Creating ${CT_HOSTNAME_VAR^} CT..."
 pct create $CTID $TEMPLATE_STRING --arch $ARCH --cores $CT_CPU_CORES --hostname $CT_HOSTNAME --cpulimit 1 --cpuunits 1024 --memory $CT_RAM \
---net0 name=eth0,bridge=vmbr0,tag=$CT_TAG,firewall=1,gw=$CT_GW,ip=$CT_IP,type=veth \
+--net0 name=eth0,bridge=vmbr0,tag=$CT_TAG,firewall=1,gw=$CT_GW,ip=$CT_IP/$CT_IP_SUBNET,type=veth \
 --ostype $OSTYPE --rootfs $STORAGE:$CT_DISK_SIZE,acl=1 --swap $CT_SWAP --unprivileged $CT_UNPRIVILEGED --onboot 1 --startup order=2 >/dev/null
