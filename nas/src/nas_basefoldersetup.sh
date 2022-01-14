65df3f64c8ebc45e888b2095a822cfa29234e0e6
ahuacate <43461046+ahuacate@users.noreply.github.com>
2022-01-14 17:55:10 +0700

    bug fix

--- nas/src/nas_basefoldersetup.sh
+++ nas/src/nas_basefoldersetup.sh
@@ -94,7 +94,7 @@ fi
 #---- Other Files ------------------------------------------------------------------
 
 # Create empty files
-touch pve_nas_basefolderlist-xtra
+touch nas_basefolderlist-xtra
 
 #---- Body -------------------------------------------------------------------------
 
@@ -103,13 +103,13 @@ section "Create and Set Folder Permissions."
 
 # Create Default Proxmox Share points
 msg_box "#### PLEASE READ CAREFULLY - SHARED FOLDERS ####\n
-Shared folders are the basic directories where you can store files and folders on your PVE NAS. Below is a list of default PVE NAS shared folders. You can create additional custom shared folders in the coming steps.
+Shared folders are the basic directories where you can store files and folders on your NAS. Below is a list of default NAS shared folders. You can create additional custom shared folders in the coming steps.
 
-$(while IFS=',' read -r var1 var2; do msg "  --  /srv/${CT_HOSTNAME}/'${var1}'\n"; done <<< $( cat pve_nas_basefolderlist | sed 's/^#.*//' | sed '/^$/d' ))"
+$(while IFS=',' read -r var1 var2; do msg "  --  /srv/${CT_HOSTNAME}/'${var1}'\n"; done <<< $( cat nas_basefolderlist | sed 's/^#.*//' | sed '/^$/d' ))"
 echo
 
 while true; do
-  if [ $(cat pve_nas_basefolderlist-xtra | wc -l) == 0 ]; then
+  if [ $(cat nas_basefolderlist-xtra | wc -l) == 0 ]; then
     read -p "Do you want to create a custom shared folder [y/n]? " -n 1 -r YN
   else
     read -p "Do you want to another custom shared folder [y/n]? " -n 1 -r YN
@@ -120,7 +120,7 @@ while true; do
       while true; do
         # Function to input dir name
         input_dirname_val
-        if [ $(cat pve_nas_basefolderlist | sed '/^#/d' | sed '/^$/d' | awk '{ print $1 }' | grep -xqFe ${DIR_NAME} > /dev/null; echo $?) == 0 ];then
+        if [ $(cat nas_basefolderlist | sed '/^#/d' | sed '/^$/d' | awk '{ print $1 }' | grep -xqFe ${DIR_NAME} > /dev/null; echo $?) == 0 ];then
           warn "There are issues with your input:\n  1. The folder '${DIR_NAME}' already exists.\n  Try again..."
           echo
         else
@@ -137,29 +137,29 @@ while true; do
       select menu in "${options[@]}"; do
         case $menu in
           "$XTRA_SHARE01")
-            echo "${DIR_NAME},Custom folder,root,0750,65608:rwx,65607:rwx" >> pve_nas_basefolderlist
-            echo "${DIR_NAME},Custom folder,root,0750,65608:rwx,65607:rwx" >> pve_nas_basefolderlist-xtra
+            echo "${DIR_NAME},Custom folder,root,0750,65608:rwx,65607:rwx" >> nas_basefolderlist
+            echo "${DIR_NAME},Custom folder,root,0750,65608:rwx,65607:rwx" >> nas_basefolderlist-xtra
             info "You have selected: ${YELLOW}Standard User${NC} for folder '${DIR_NAME}'."
             echo
             break
             ;;
           "$XTRA_SHARE02")
-            echo "${DIR_NAME},Custom folder,root,0750,65605:rwx,65607:rwx" >> pve_nas_basefolderlist
-            echo "${DIR_NAME},Custom folder,root,0750,65605:rwx,65607:rwx" >> pve_nas_basefolderlist-xtra
+            echo "${DIR_NAME},Custom folder,root,0750,65605:rwx,65607:rwx" >> nas_basefolderlist
+            echo "${DIR_NAME},Custom folder,root,0750,65605:rwx,65607:rwx" >> nas_basefolderlist-xtra
             info "You have selected: ${YELLOW}Medialab${NC} for folder '${DIR_NAME}'."
             echo
             break
             ;;
           "$XTRA_SHARE03")
-            echo "${DIR_NAME},Custom folder,root,0750,65606:rwx,65607:rwx" >> pve_nas_basefolderlist
-            echo "${DIR_NAME},Custom folder,root,0750,65606:rwx,65607:rwx" >> pve_nas_basefolderlist-xtra
+            echo "${DIR_NAME},Custom folder,root,0750,65606:rwx,65607:rwx" >> nas_basefolderlist
+            echo "${DIR_NAME},Custom folder,root,0750,65606:rwx,65607:rwx" >> nas_basefolderlist-xtra
             info "You have selected: ${YELLOW}Homelab${NC} for folder '${DIR_NAME}'."
             echo
             break
             ;;
           "$XTRA_SHARE04")
-            echo "${DIR_NAME},Custom folder,root,0750,65607:rwx" >> pve_nas_basefolderlist
-            echo "${DIR_NAME},Custom folder,root,0750,65607:rwx" >> pve_nas_basefolderlist-xtra
+            echo "${DIR_NAME},Custom folder,root,0750,65607:rwx" >> nas_basefolderlist
+            echo "${DIR_NAME},Custom folder,root,0750,65607:rwx" >> nas_basefolderlist-xtra
             info "You have selected: ${YELLOW}Privatelab${NC} for folder '${DIR_NAME}'."
             echo
             break
@@ -169,7 +169,7 @@ while true; do
       done
       ;;
     [Nn]*)
-      if [ $(cat pve_nas_basefolderlist-xtra | wc -l) == 0 ]; then
+      if [ $(cat nas_basefolderlist-xtra | wc -l) == 0 ]; then
         XTRA_SHARES=1
         info "You have chosen not create any custom shared folders."
       else
@@ -189,7 +189,7 @@ done
 # Create Proxmox ZFS Share points
 msg "Creating $SECTION_HEAD base /$POOL/$HOSTNAME folder shares..."
 echo
-cat pve_nas_basefolderlist | sed '/^#/d' | sed '/^$/d' >/dev/null > pve_nas_basefolderlist_input
+cat nas_basefolderlist | sed '/^#/d' | sed '/^$/d' >/dev/null > nas_basefolderlist_input
 while IFS=',' read -r dir desc group permission acl_01 acl_02 acl_03 acl_04 acl_05; do
   if [ -d "${DIR_SCHEMA}/${dir}" ]; then
     info "Pre-existing folder: ${UNDERLINE}"${DIR_SCHEMA}/${dir}"${NC}\n  Setting ${group} group permissions for existing folder."
@@ -235,19 +235,19 @@ while IFS=',' read -r dir desc group permission acl_01 acl_02 acl_03 acl_04 acl_
     fi
     echo
   fi
-done < pve_nas_basefolderlist_input
+done < nas_basefolderlist_input
 
 # # Chattr set ZFS share points attributes to +a
 # while read -r dir group permission acl_01 acl_02 acl_03 acl_04 acl_05; do
 #   chattr +a "${DIR_SCHEMA}/${dir}"
-# done < pve_nas_basefolderlist_input
+# done < nas_basefolderlist_input
 
 
 # Create Default SubFolders
-if [ -f pve_nas_basefoldersubfolderlist ]; then
+if [ -f nas_basefoldersubfolderlist ]; then
   msg "Creating $SECTION_HEAD subfolder shares..."
   echo
-  echo -e "$(eval "echo -e \"`<pve_nas_basefoldersubfolderlist`\"")" | sed '/^#/d' | sed '/^$/d' >/dev/null > pve_nas_basefoldersubfolderlist_input
+  echo -e "$(eval "echo -e \"`<nas_basefoldersubfolderlist`\"")" | sed '/^#/d' | sed '/^$/d' >/dev/null > nas_basefoldersubfolderlist_input
   while IFS=',' read -r dir group permission acl_01 acl_02 acl_03 acl_04 acl_05; do
     if [ -d "${dir}" ]; then
       info "${dir} exists.\n  Setting ${group} group permissions for this folder."
@@ -292,13 +292,13 @@ if [ -f pve_nas_basefoldersubfolderlist ]; then
       fi
       echo
     fi
-  done < pve_nas_basefoldersubfolderlist_input
+  done < nas_basefoldersubfolderlist_input
   # Chattr set ZFS share points attributes to +a
   while IFS=',' read -r dir group permission acl_01 acl_02 acl_03 acl_04 acl_05; do
     touch ${dir}/.foo_protect
     chattr +i ${dir}/.foo_protect
     # chmod +t ${dir}/.foo_protect
-  done < pve_nas_basefoldersubfolderlist_input
+  done < nas_basefoldersubfolderlist_input
 fi
 
 #---- Finish Line ------------------------------------------------------------------
