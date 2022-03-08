6ee4e7d668b27c807594a50f6cf48cc2976e4a8a
ahuacate <43461046+ahuacate@users.noreply.github.com>
2022-03-08 11:14:29 +0700

    update

--- bash/src/pve_repo_loader.sh
+++ bash/src/pve_repo_loader.sh
@@ -26,11 +26,17 @@ fi
 
 #---- Download Github repo
 if [ ! -f /mnt/pve/nas-*[0-9]-git/${GIT_USER}/developer_settings.git ]; then
-  # Download packages
+  # Download Repo packages
   wget -qL - ${GIT_SERVER}/${GIT_USER}/${GIT_REPO}/archive/${GIT_BRANCH}.tar.gz -O ${REPO_TEMP}/${GIT_REPO}.tar.gz
   tar -zxf ${REPO_TEMP}/${GIT_REPO}.tar.gz -C ${REPO_TEMP}
   mv ${REPO_TEMP}/${GIT_REPO}-${GIT_BRANCH} ${REPO_TEMP}/${GIT_REPO}
   chmod -R 777 ${REPO_TEMP}/${GIT_REPO}
+  # Download Common packages
+  wget -qL - ${GIT_SERVER}/${GIT_USER}/common/archive/${GIT_BRANCH}.tar.gz -O ${REPO_TEMP}/common.tar.gz
+  tar -zxf ${REPO_TEMP}/common.tar.gz -C ${REPO_TEMP}
+  mv ${REPO_TEMP}/common-${GIT_BRANCH}/ ${REPO_TEMP}/common
+  mv ${REPO_TEMP}/common/ ${REPO_TEMP}/${GIT_REPO}
+  chmod -R 777 ${REPO_TEMP}/${GIT_REPO}/common
   # Create new tar files
   rm ${REPO_TEMP}/${GIT_REPO}.tar.gz
   tar --exclude=".*" -czf ${REPO_TEMP}/${GIT_REPO}.tar.gz -C ${REPO_TEMP} ${GIT_REPO}/
