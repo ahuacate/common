434126bdc85bc4bebc24979f4167b3796a8ed698
ahuacate <43461046+ahuacate@users.noreply.github.com>
2022-08-06 16:45:22 +0700

    update

--- pve/src/pvesource_set_allvmvars.sh
+++ pve/src/pvesource_set_allvmvars.sh
@@ -973,7 +973,7 @@ fi
 
 
 #---- Set Nameserver IP Address ( DNS )
-if [ ${TAG} != '0' ] && [[ ${IP} =~ ${ip4_regex} ]] && [ $(ip route show default | awk '/default/ {print $3}' | awk -F'.' '{ print $3 }') != ${TAG} ]; then
+if [ ! ${TAG} == '0' ] && [[ ${IP} =~ ${ip4_regex} ]] && [ $(ip route show default | awk '/default/ {print $3}' | awk -F'.' '{ print $3 }') != ${TAG} ]; then
   # Nameserver - match to PVE host IP format ( for IPv4 only )
   nameserver_octet3=$TAG
   nameserver_octet4=$(ip route show default | awk '/default/ {print $3}' | awk -F'.' '{ print $4 }')
@@ -1021,7 +1021,7 @@ if [ ${TAG} != '0' ] && [[ ${IP} =~ ${ip4_regex} ]] && [ $(ip route show default
       echo
     fi
   done
-elif [ ! ${TAG} == '0' ] && [[ ${IP} =~ ${ip4_regex} ]] && [[ $(ip route show default | awk '/default/ {print $3}' | awk -F'.' '{ print $3 }') == ${TAG}]]; then
+elif [ ! ${TAG} == '0' ] && [[ ${IP} =~ ${ip4_regex} ]] && [ $(ip route show default | awk '/default/ {print $3}' | awk -F'.' '{ print $3 }') == ${TAG}]; then
   # Set nameserver to match host (same vlan)
   NAMESERVER=$(ip route show default | awk '/default/ {print $3}')
 elif [ ${TAG} == '0' ]; then
