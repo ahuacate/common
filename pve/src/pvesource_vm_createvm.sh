d92f09957a63d74e5e16e55906723f7aa424ac5f
ahuacate <43461046+ahuacate@users.noreply.github.com>
2022-10-22 10:27:19 +0700

    update

--- pve/src/pvesource_vm_createvm.sh
+++ pve/src/pvesource_vm_createvm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # ----------------------------------------------------------------------------------
 # Filename:     pvesource_vm_createvm.sh
-# Description:  Source script for creating PVE VM containers
+# Description:  Source script for creating PVE VM machines
 # ----------------------------------------------------------------------------------
 
 #---- Source -----------------------------------------------------------------------
@@ -14,10 +14,6 @@ OS_DIST=${VM_OS_DIST}
 OSVERSION=$(echo ${VM_OSVERSION} | sed 's/[.|_]//') # Remove and '. or '_' from version number
 OTHER_OS_URL=${VM_OTHER_OS_URL}
 
-# Regex for Ipv4 and IPv6
-ip4_regex='^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
-ip6_regex='^([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}$'
-
 #---- Other Variables --------------------------------------------------------------
 #---- Other Files ------------------------------------------------------------------
 
@@ -29,14 +25,17 @@ UBUNTU_2004_URL="https://cloud-images.ubuntu.com/focal/current/focal-server-clou
 UBUNTU_2110_URL="https://cloud-images.ubuntu.com/impish/current/impish-server-cloudimg-amd64.img"
 UBUNTU_2204_URL="https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
 
-
 #---- Functions --------------------------------------------------------------------
 
-# Read variable file list
+#---- printsection
 printsection() {
-  # Example: $(printSection SECTION_NAME | sed -n 's/^name//p/' < /etc/applications.conf)
-  section="$1"
+  # Read variable file list by section.
+  # Example: $(printsection section_name | sed -n 's/^name//p/' < /etc/applications.conf)
+
+  # Local args
+  local  section="$1"
   found=false
+
   # Run function
   while read line
   do
@@ -47,24 +46,143 @@ printsection() {
   done
 }
 
-# Spinner takes the pid of the process as the first argument and
-# string to display as second argument (default provided) and spins
-# until the process completes.
-# Usage command && spinner $!
-spinner() {
-    local PROC="$1"
-    local str="${2:-Working...}"
-    local delay="0.1"
-    tput civis  # hide cursor
-    while [ -d /proc/$PROC ]; do
-        printf '\033[s\033[u[ / ] %s\033[u' "$str"; sleep "$delay"
-        printf '\033[s\033[u[ — ] %s\033[u' "$str"; sleep "$delay"
-        printf '\033[s\033[u[ \ ] %s\033[u' "$str"; sleep "$delay"
-        printf '\033[s\033[u[ | ] %s\033[u' "$str"; sleep "$delay"
+#---- create_section_category_LIST
+create_section_category_LIST() {
+  # Get all section category names.
+  # Creates array named: section_category_LIST
+
+  # Unset list name
+  section_category_LIST=()
+
+  # Run function
+  while read line
+  do
+    if [[ "$line" =~ ^\#[\-]{4}\[(CT|VM)_[\-A-Z0-9_]+_OPTIONS\]$ ]]; then
+      section_category_LIST+=( "$(echo "$line" | sed -E 's/(^|\])[^[]*($|\[)//g')" )
+    fi
+  done < ${PRESET_VAR_SRC}
+}
+# create_section_category_LIST
+# printf '%s\n' "${section_category_LIST[@]}"
+
+
+#---- make_vm_create_LIST
+function make_vm_create_LIST() {
+  # Requires function 'printsection'
+  # Requires src file input: "$PRESET_VAR_SRC" must be set.
+  # To run example: make_vm_create_LIST "VM_NET_OPTIONS" "net_LIST"
+  # "section_name" and "list_name" must be included.
+  # If OPTION_STATUS='1:0' a single option value list is created:
+  #   --name=nas-01
+  #   --description='OMV NAS Appliance'
+  #   --onboot=1
+  # If OPTION_STATUS='1:net' a string option "--net" value list is created:
+  #   --net ip=dhcp,gw=192.168.1.15
+  # If OPTION_STATUS='0:0' the section is skipped. No array list is processed.
+
+  # Unset list name.
+  # unset ${string_name}
+
+  # Local args
+  local  section_name=$1
+  local  list_name=$2
+
+  # Create section list
+  arr_LIST=()
+  results_LIST=()
+
+  while IFS== read var value
+  do
+    eval $var=$value
+    eval i='$'$var
+
+    # Check enable option
+    if [[ "$var" =~ ^OPTION_STATUS$ ]] && [[ "$i" =~ ^0\:.*$ ]]; then
+      break
+    elif [[ "$var" =~ ^OPTION_STATUS$ ]] && [[ "$i" =~ ^1\:[\-a-z_]+[0-9]?+$ ]]; then
+      string_name=$(echo $i | awk -F':' '{ print $NF }')
+      arr_LIST+=( "--${string_name}" )
+      continue
+    elif [[ "$var" =~ ^OPTION_STATUS$ ]] && [[ "$i" =~ ^1\:0$ ]]; then
+      unset string_name
+      continue
+    fi
+    # Make array value
+    if [ -n "${i}" ]; then
+      j=$(echo ${var} | sed -e "s/^\(VM\|CT\)_//i")
+      if [ -n "${string_name}" ]; then
+        # SCSI args
+        if [[ "${string_name}" =~ ^scsi[0-9]$ ]] && [[ "${j}" =~ ^SCSI[0-9]_SIZE$ ]]; then
+          arr_LIST+=( "${VOLUME}:${i}" )
+        elif [[ "${string_name}" =~ ^scsi[0-9]$ ]] && [[ "${j}" =~ ^SCSI[0-9]_[A-Z]+$ ]]; then
+          arr_LIST+=( "$(echo ${j,,} | sed -e "s/^\(SCSI[0-9]\)_//i")=${i}" )
+        # CDROM args
+        elif [[ "${string_name}" =~ ^cdrom$ ]] && [[ "${j}" =~ ^ISO_SRC$ ]]; then
+          arr_LIST+=( "${OS_TMPL}" )
+        # SCSI args
+        elif [[ "${string_name}" =~ ^scsi[0-9]$ ]] && [[ "${j}" =~ ^SCSI[0-9]_SIZE$ ]]; then
+          arr_LIST+=( "${VOLUME}:${i}" )
+        # IPv4 args
+        elif [[ "${string_name}" =~ ^ipconfig[0-9]$ ]] && [[ "${j}" =~ ^IP$ ]] && [[ "${i}" =~ ${ip4_regex} ]]; then
+          arr_LIST+=( "${j,,}=${i}/${CIDR}" )
+        # IPv6 args
+        elif [[ "${string_name}" =~ ^ipconfig[0-9]$ ]] && [[ "${j}" =~ ^IP$ ]] && [[ "${i}" =~ ${ip6_regex} ]]; then
+          arr_LIST+=( "${j,,}=${i}/${CIDR6}" )
+        # Standard string
+        else
+          arr_LIST+=( "$(echo "${j,,}=${i}")" )
+        fi
+      else
+        arr_LIST+=( "$(echo "--${j,,} ${i}")" )
+      fi
+    fi
+  done <<< $(printsection ${section_name} < ${PRESET_VAR_SRC})
+
+  # Assemble 'results' array
+  if [ "${#arr_LIST[@]}" != '0' ]; then
+    if [ -n "${string_name}" ]; then
+      # Add arr string options to results_LIST
+      results_LIST+=( "$(echo "${arr_LIST[*]}" | sed 's/ /,/2g')" )
+    else
+      # Add single options to results_LIST
+      for i in "${arr_LIST[@]}"
+      do 
+        results_LIST+=( "$(echo "$i")" )
+      done
+    fi
+  fi
+
+  # Create list by name
+  if [ -n "${list_name}" ]; then
+    name=${list_name}
+    unset $name
+    for ((i=0; i<${#results_LIST[@]}; ++i)) ; do
+        read "$name[$i]" <<< "${results_LIST[i]}"
     done
-    printf '\033[s\033[u%*s\033[u\033[0m' $((${#str}+6)) " "  # return to normal
-    tput cnorm  # restore cursor
-    return 0
+  fi
+}
+# make_vm_create_LIST "VM_GENERAL_OPTIONS" "test_LIST"
+# printf '%s\n' "${test_LIST[@]}"
+
+#---- Spinner
+spinner() {
+  # Spinner takes the pid of the process as the first argument and
+  # string to display as second argument (default provided) and spins
+  # until the process completes.
+  # Usage command && spinner $!
+  local PROC="$1"
+  local str="${2:-Working...}"
+  local delay="0.1"
+  tput civis  # hide cursor
+  while [ -d /proc/$PROC ]; do
+    printf '\033[s\033[u[ / ] %s\033[u' "$str"; sleep "$delay"
+    printf '\033[s\033[u[ — ] %s\033[u' "$str"; sleep "$delay"
+    printf '\033[s\033[u[ \ ] %s\033[u' "$str"; sleep "$delay"
+    printf '\033[s\033[u[ | ] %s\033[u' "$str"; sleep "$delay"
+  done
+  printf '\033[s\033[u%*s\033[u\033[0m' $((${#str}+6)) " "  # return to normal
+  tput cnorm  # restore cursor
+  return 0
 }
 
 #---- Body -------------------------------------------------------------------------
@@ -124,8 +242,6 @@ if [ -n "${OTHER_OS_URL}" ]; then
   fi
 fi
 
-# OS_TMPL="$(find ${OS_TMPL_PATH} -type f)"
-
 # VM Install dir location
 rootdir_LIST=( $(pvesm status --content rootdir -enabled | awk 'NR>1 {print $1}') )
 if [ ${#rootdir_LIST[@]} -eq '0' ]; then
@@ -147,248 +263,23 @@ elif [ ${#rootdir_LIST[@]} -gt '1' ]; then
 fi
 
 
-#---- Validate & set architecture dependent variables
-ARCH=$(dpkg --print-architecture)
-
 #---- Create VM input arrays
-# general_LIST array
-unset general_LIST
-while IFS== read var value
+PRESET_VAR_SRC='/mnt/pve/nas-01-git/ahuacate/pve-nas/src/omv/pve_nas_vm_nas_installer.sh'
+# Create default lists
+create_section_category_LIST
+
+# Create VM run script
+input_LIST=()
+input_LIST+=( "qm create ${VMID}" )
+while read line
 do
-  eval i='$'$var
-  if [ -n "${i}" ]; then
-    # Wrap Description var in quotes
-    if [ "${var}" == 'DESCRIPTION' ]; then
-      i=\"${i}\"
-    fi
-    general_LIST+=( "$(echo "--${var,,} ${i}")" )
+  # Run func 
+  make_vm_create_LIST "$line"
+  # Add results to input_LIST
+  if [ "${#results_LIST[@]}" != '0' ]; then
+    input_LIST+=( "$(printf '%s\n' "${results_LIST[@]}")" )
   fi
-done <<< $(printsection COMMON_GENERAL_OPTIONS < ${PRESET_VAR_SRC})
-printf '%s\n' "${general_LIST[@]}"
-while IFS== read var value
-do
-  eval i='$'$var
-  if [ -n "${i}" ]; then
-    j=$(echo ${var} | sed 's/^VM_//')
-    general_LIST+=( "$(echo "--${j,,} ${i}")" )
-  fi
-done <<< $(printsection VM_GENERAL_OPTIONS < ${PRESET_VAR_SRC})
-printf '%s\n' "${general_LIST[@]}"
+done <<< $(printf '%s\n' "${section_category_LIST[@]}")
+printf '%s\n' "${input_LIST[@]}" | sed '$ ! s/$/ \\ /'
 
-# scsi0_LIST
-unset scsi0_LIST
-scsi0_LIST+=$(echo "--scsi0")
-scsi0_LIST+=( "$(echo "${VOLUME}:${VM_SCSI0_SIZE}")" )
-while IFS== read var value
-do
-  eval i='$'$var
-  if [ -n "${i}" ]; then
-    # Ignore VM_SCSI0_SIZE
-    if [ "${var}" == 'VM_SCSI0_SIZE' ]; then 
-      continue
-    fi
-    j=$(echo ${var} | sed 's/^VM_SCSI0_//')
-    scsi0_LIST+=( "$(echo "${j,,}=${i}")" )
-  fi
-done <<< $(printsection VM_SCSI0_OPTIONS < ${PRESET_VAR_SRC} | grep -v '^VM_SCSI0_SIZE.*')
-
-# scsi1_LIST
-unset scsi1_LIST
-scsi0_LIST+=$(echo "--scsi1")
-scsi0_LIST+=( "$(echo "${VOLUME}:${VM_SCSI1_SIZE}")" )
-while IFS== read var value
-do
-  eval i='$'$var
-  if [ -n "${i}" ]; then
-    # Ignore VM_SCSI1_SIZE
-    if [ "${var}" == 'VM_SCSI1_SIZE' ]; then 
-      continue
-    fi
-    j=$(echo ${var} | sed 's/^VM_SCSI1_//')
-    scsi1_LIST+=( "$(echo "${j,,}=${i}")" )
-  fi
-done <<< $(printsection VM_SCSI1_OPTIONS < ${PRESET_VAR_SRC} | grep -v '^VM_SCSI1_SIZE.*')
-
-# net_LIST
-unset net_LIST
-net_LIST+=$(echo "--net0")
-while IFS== read var value
-do
-  eval i='$'$var
-  if [ -n "${i}" ]; then
-    # Ignore of tag=(0|1)
-    if [ "${var}" == 'TAG' ] && [[ ${i} =~ (0|1) ]]; then 
-      continue
-    fi
-    net_LIST+=( "$(echo "${var,,}=${i}")" )
-  fi
-done <<< $(printsection COMMON_NET_OPTIONS < ${PRESET_VAR_SRC})
-# while IFS== read var value
-# do
-#   eval i='$'$var
-#   if [ -n "${i}" ]; then
-#     j=$(echo ${var} | sed 's/^VM_//')
-#     net_LIST+=( "$(echo "${j,,}=${i}")" )
-#   fi
-# done <<< $(printsection VM_NET_OPTIONS < ${PRESET_VAR_SRC})
-
-# startup_LIST
-unset startup_LIST
-startup_LIST+=$(echo "--startup")
-while IFS== read var value
-do
-  eval i='$'$var
-  if [ -n "${i}" ]; then
-    j=$(echo ${var} | sed 's/^VM_//')
-    startup_LIST+=( "$(echo "${j,,}=${i}")" )
-  fi
-done <<< $(printsection VM_STARTUP_OPTIONS < ${PRESET_VAR_SRC})
-
-# cloudinit_LIST
-unset cloudinit_LIST
-cloudinit_LIST=()
-while IFS== read var value
-do
-  eval i='$'$var
-  if [ -n "${i}" ]; then
-    j=$(echo ${var} | sed 's/^CT_//')
-    cloudinit_LIST+=( "$(echo "${j,,}=${i}")" )
-  fi
-done <<< $(printsection VM_CLOUD_INIT < ${PRESET_VAR_SRC})
-
-# cloudinit_ipconfig_LIST
-unset cloudinit_ipconfig_LIST
-cloudinit_ipconfig_LIST+=$(echo "--ipconfig0")
-while IFS== read var value
-do
-  eval i='$'$var
-  if [ -n "${i}" ]; then
-    # Add CIDR value to IP/IP6
-    if [ "${var}" == 'IP' ] && [[ $i =~ ${ip4_regex} ]]; then
-      i=$(echo ${i} | sed "s/$/\/${CIDR}/")
-    elif [ "${var}" == 'IP' ] || [ "${var}" == 'IP6' ] && [[ $i =~ 'dhcp' ]]; then
-      i=$(echo ${i})
-    elif [ "${var}" == 'IP6' ] && [[ $i =~ ${ip6_regex} ]]; then
-      i=$(echo ${i} | sed "s/$/\/${CIDR6}/")
-    fi
-    j=$(echo ${var} | sed 's/^CT_//')
-    cloudinit_ipconfig_LIST+=( "$(echo "${j,,}=${i}")" )
-  fi
-done <<< $(printsection VM_CLOUD_INIT_IPCONFIG < ${PRESET_VAR_SRC})
-
-
-#---- Create VM
-# Create VM variables
-unset qm_create_LIST
-qm_create_LIST=()
-# Set VMID
-qm_create_LIST+=( "$(echo "${VMID}")" )
-# general_LIST vars
-if [ ${#general_LIST[@]} -ge '1' ]; then
-  qm_create_LIST+=( "$(printf '%s\n' "${general_LIST[@]}" | sed 's/$//')" )
-fi
-# net_LIST vars
-qm_create_LIST+=( "$(printf '%s\n' "${net_LIST[@]}" | xargs | sed 's/ /,/2g')" )
-# startup_LIST vars
-if [ ${#startup_LIST[@]} -ge '2' ]; then
-  qm_create_LIST+=( "$(printf '%s\n' "${startup_LIST[@]}" | xargs | sed 's/ /,/2g')" )
-fi
-# scsi0_LIST vars
-if [ ${#scsi0_LIST[@]} -ge '2' ]; then
-  qm_create_LIST+=( "$(printf '%s\n' "${scsi0_LIST[@]}" | xargs | sed 's/ /,/2g')" )
-fi
-# scsi1_LIST vars
-if [ ${#scsi1_LIST[@]} -ge '2' ]; then
-  qm_create_LIST+=( "$(printf '%s\n' "${scsi1_LIST[@]}" | xargs | sed 's/ /,/2g')" )
-fi
-# Set IDE/CDROM
-qm_create_LIST+=( "$(echo "--ide2 ${OS_TMPL},media=cdrom")" )
-# # Create VM
-# msg "Creating ${HOSTNAME^} VM..."
-# qm create $(printf '%s ' "${pct_create_LIST[@]}" | sed 's/$//')
-# echo
-
-
-printf '%s ' "${pct_create_LIST[@]}"
-
-
-
-
-
-# # Create VM
-# msg "Creating PVE ${VM_OSTYPE^} VM..."
-# qm create ${VMID} \
-# --name ${VM_HOSTNAME} \
-# --bios seabios \
-# --sockets ${VM_CPU_SOCKETS} \
-# --cores ${VM_CPU_CORES} \
-# --vcpus ${VM_VCPU} \
-# --cpulimit ${VM_CPU_LIMIT} \
-# --cpuunits ${VM_CPU_UNITS} \
-# --ostype ${VM_OS_TYPE} \
-# --memory ${VM_RAM} \
-# --balloon ${VM_RAM_BALLOON} \
-# --nameserver ${VM_DNS_SERVER} \
-# --net0 ${VM_NET_MODEL},bridge=${VM_NET_BRIDGE},firewall=${VM_NET_FIREWALL}$(if [ ${VM_NET_MAC_ADDRESS} != 'auto' ]; then echo ",macaddr=${VM_NET_MAC_ADDRESS}"; fi)$(if [ ${VM_TAG} -gt 1 ]; then echo ",tag=${VM_TAG}"; fi) \
-# --scsihw virtio-scsi-single \
-# --scsi0 ${STORAGE}:${VM_DISK_SIZE} \
-# --ide2 ${TEMPLATE_STRING},media=cdrom \
-# --autostart ${VM_AUTOSTART} \
-# --onboot ${VM_ONBOOT} \
-# --start ${VM_START} \
-# $(if [ ${VM_STARTUP_ORDER} > 0 ]; then echo "--startup order=${VM_STARTUP_ORDER}"; fi)$(if [ ${VM_STARTUP_ORDER} > 0 ] && [ ${VM_STARTUP_DELAY} -gt 0 ]; then echo ",up=${VM_STARTUP_DELAY}"; fi) >/dev/null
-
-# #qm set --ipconfig0 gw=${VM_GW},ip=${VM_IP}/${VM_IP_SUBNET}
-
-# # Checking VM Status
-# n=0
-# until [ "$n" -ge 5 ]
-# do
-#   if [ "$(qm list | grep -w ${VMID} > /dev/null; echo $?)" = 0 ]; then
-#     if [ "$(qm status ${VMID})" == "status: stopped" ]; then
-#       info "${VM_HOSTNAME^} CT has been created. Current status: ${YELLOW}$(qm status ${VMID} | awk '{print $2}')${NC}"
-#       echo
-#       break
-#     elif [ "$(qm status ${VMID})" == "status: running" ]; then
-#       info "${VM_HOSTNAME^} CT has been created. Current status: ${YELLOW}$(qm status ${VMID}| awk '{print $2}')${NC}"
-#       echo
-#       break
-#     fi
-#   elif [ "$(qm list | grep -w ${VMID} > /dev/null; echo $?)" != 0 ] && [ "$n" == 4 ]; then
-#     warn "Something went wrong creating the PVE VM. ${VM_HOSTNAME^} VM has NOT been created.\nAborting this installation in 2 seconds..."
-#     sleep 1
-#     echo
-#     # exit 0
-#   fi
-#   n=$((n+1)) 
-#   sleep 1
-# done
-
-# # Test Run
-# VMID='120'
-# VM_OS_TYPE='l26'
-# VM_HOSTNAME='nas-04'
-# VM_CPU_UNITS='1024'
-# VM_CPU_LIMIT='0'
-# VM_CPU_SOCKETS='1'
-# VM_CPU_CORES='1'
-# VM_VCPU='1'
-# VM_RAM='1024'
-# VM_RAM_BALLOON='512'
-# VM_DNS_SERVER='192.168.1.5'
-# VM_TAG='1'
-# VM_GW='192.168.1.5'
-# VM_IP='192.168.1.77'
-# VM_IP_SUBNET='24'
-# VM_NET_BRIDGE='vmbr0'
-# VM_NET_MODEL='virtio'
-# VM_NET_MAC_ADDRESS='auto'
-# VM_NET_FIREWALL='1'
-# VM_DISK_SIZE='10'
-# VM_ISO='TrueNAS-12.0-U5.1.iso'
-# VM_AUTOSTART='1'
-# VM_ONBOOT='1'
-# VM_STARTUP_ORDER='1'
-# VM_STARTUP_DELAY='30'
-# VM_START='0'
-# SRC_ISO_URL='https://download.freenas.org/latest/x64/TrueNAS-12.0-U5.1.iso'
+#-----------------------------------------------------------------------------------
