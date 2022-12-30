28f37e7668092bc2ce84c1ce9cc0da1345a35691
ahuacate <43461046+ahuacate@users.noreply.github.com>
2022-12-30 14:46:39 +0700

    update

---
+++ bash/src/selftar.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# ----------------------------------------------------------------------------------
+# Filename:     selftar.sh
+# Description:  This script is for creating self-executing tar packages
+# Shout-out:    https://github.com/alexradzin
+# ----------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------
+# Script:
+# It is a good idea to create script that takes regular tar.gz and creates
+# self-extracting archive. The script is available here.
+# It accepts the following arguments:
+#
+#     mandatory path to tar.gz 
+#     optional command that is automatically executed right after the archive
+#     extracting. Typically it is script packaged into the tar. 
+#
+# The name of resulting executable is as name of tar.gz with suffix ".run".
+#
+# Usage Examples: 
+#
+# Create self-extracted executable: 
+#     ./selftar.sh my.tar.gz
+#
+# Create self-extracted executable with command executed after archive extracting: 
+#     ./selftar.sh my.tar.gz "folder/install.sh"
+#
+# Both examples create executable file my.tar.gz.run that extracts files initially
+# packaged to my.tar.gz to current directory:
+#     ./my.tar.gz.run
+#
+# Example to run (include brackets):
+#     (mkdir -p /tmp/selftar ; cd /tmp/selftar ; /tmp/installer_pkg.tar.gz.run)
+# Note: You can rename Selftar output file to whatever your want:
+#     installer_pkg.tar.gz.run --> installer.run
+# ----------------------------------------------------------------------------------
+
+if [ $# -eq 0 ]; then
+  echo "This script creates self extractable executable"
+  echo Usage: $0 TAR.GZ [COMMAND]
+  exit;
+fi
+if [ $# -gt 0 ]; then
+  TAR_FILE=$1
+fi
+EXIT_COMMAND=exit
+if [ $# -gt 1 ]; then
+  EXIT_COMMAND="exec $2"
+fi
+
+SELF_EXTRACTABLE="$TAR_FILE.run"
+
+echo '#!/bin/sh' > $SELF_EXTRACTABLE
+echo 'dd bs=1 skip=`head -3 $0 | wc -c`  if=$0  | gunzip -c  | tar -x' >> $SELF_EXTRACTABLE
+echo "$EXIT_COMMAND" >> $SELF_EXTRACTABLE
+
+cat $TAR_FILE >> $SELF_EXTRACTABLE
+chmod a+x $SELF_EXTRACTABLE
+#-----------------------------------------------------------------------------------
---
+++ pve/src/pvesource_send_email.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+# ----------------------------------------------------------------------------------
+# Filename:     pvesource_send_email.sh
+# Description:  Source script for "Introduction" body text at opening of VM Script
+#
+# You can run this script with the following arguments:
+#   -t --to
+#   -c --cc
+#   -b --bcc
+#   -s --subject
+#   -h --html
+#   -a --attach
+#   ./pvesource_send_email.sh -t "hello@gmail.com" -c CC -b BCC -s
+#
+# ----------------------------------------------------------------------------------
+
+#---- Source -----------------------------------------------------------------------
+#---- Dependencies -----------------------------------------------------------------
+#---- Static Variables -------------------------------------------------------------
+#---- Other Variables --------------------------------------------------------------
+#---- Other Files ------------------------------------------------------------------
+#---- Function ---------------------------------------------------------------------
+
+# Check PVE host SMTP status
+function check_smtp_status() {
+  # Host SMTP Option ('0' is inactive, '1' is active)
+  var='ahuacate_smtp'
+  file='/etc/postfix/main.cf'
+  if [ -f $file ] && [ "$(systemctl is-active --quiet postfix; echo $?)" == '0' ]; then
+    SMTP_STATUS=$(grep --color=never -Po "^${var}=\K.*" "${file}" || true)
+  else
+    # Set SMTP inactive
+    SMTP_STATUS=0
+fi
+}
+
+#---- Body -------------------------------------------------------------------------
+
+#---- Prerequisites
+
+# Check PVE host SMTP status
+# '0' inactive, '1' enabled.
+check_smtp_status
+if [ "$SMTP_STATUS" == '0' ]
+then
+  # Options if SMTP is inactive
+  display_msg='Unfortunately we cannot send this email. The SMTP server in your network is not known. Your PVE hosts should be configured to support SMTP email services. Our PVE Host SMTP Server installer is available in our PVE Host Toolbox located at GitHub:\n\n    --  https://github.com/ahuacate/pve-host'
+
+  msg_box "#### PLEASE READ CAREFULLY ####\n\n$(echo ${display_msg})"
+  sleep 3
+  return
+fi
+
+#---- Body
+
+# Parse the command line arguments
+while getopts ":t:c:b:s:h:a:" opt
+do
+  case $opt in
+    t|--to) to=$OPTARG;;
+    c|--cc) cc=$OPTARG;;
+    b|--bcc) bcc=$OPTARG;;
+    s|--subject) subject=$OPTARG;;
+    h|--html) html_file=$OPTARG;;
+    a|--attach) attachments+=("$OPTARG");;
+    \?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
+    :) echo "Option -$OPTARG requires an argument." >&2; exit 1;;
+  esac
+done
+
+# Check if the required arguments were supplied
+if [[ -z $to || -z $subject || -z $html_file ]]
+then
+    echo "Usage: $0 -t TO -c CC -b BCC -s SUBJECT -h HTML_FILE -a ATTACHMENT_FILE..."
+    exit 1
+fi
+
+# Set the content type to HTML
+content_type="Content-Type: text/html; charset=UTF-8"
+
+# Begin constructing the message
+message=$(cat <<EOF
+To: $to
+Cc: $cc
+Bcc: $bcc
+Subject: $subject
+MIME-Version: 1.0
+$content_type
+
+EOF
+)
+
+# Add the HTML body to the message
+message=$message$(cat $html_file)
+
+# If there are attachments, add them to the message
+if [ ${#attachments[@]} -gt 0 ]
+then
+    boundary="unique-boundary-$RANDOM"
+
+    # Add the attachments to the message
+    for attachment in "${attachments[@]}"; do
+        # Encode the attachment as base64
+        encoded_attachment=$(base64 "$attachment" | tr -d '\n')
+        attachment_name=$(basename "$attachment")
+
+        message=$message$(cat <<EOF
+--$boundary
+Content-Type: application/octet-stream; name="$attachment_name"
+Content-Disposition: attachment; filename="$attachment_name"
+Content-Transfer-Encoding: base64
+
+$encoded_attachment
+EOF
+)
+    done
+
+    # Add the closing boundary to the message
+    message=$message"--$boundary--"
+fi
+
+# Send the email using sendmail
+echo "$message" | sendmail -t
+#-----------------------------------------------------------------------------------
+
+
+
