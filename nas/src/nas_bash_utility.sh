267eb8aab1758714c6be3aa1d21a74ae7cc1bef9
ahuacate <43461046+ahuacate@users.noreply.github.com>
2023-02-16 17:58:32 +0700

    update

--- nas/src/nas_bash_utility.sh
+++ nas/src/nas_bash_utility.sh
@@ -202,7 +202,7 @@ function storage_list() {
     var8=$(lsblk -nbrd -o SIZE $dev | awk '{ $1=sprintf("%.0f",$1/(1024^3))"G" } {print $0}')
 
     # Rota (Col 10)
-    if [ $(hdparm -I $dev 2> /dev/null | awk -F':' '/Nominal Media Rotation Rate/ { print $2 }' | sed 's/ //g') = 'SolidStateDevice' ]
+    if [[ $(hdparm -I $dev 2> /dev/null | awk -F':' '/Nominal Media Rotation Rate/ { print $2 }' | sed 's/ //g') == 'SolidStateDevice' ]]
     then
       var10=0
     else
