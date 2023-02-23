65d6ab5875415f6da9f88b7b2d3511c30a30a82a
ahuacate <43461046+ahuacate@users.noreply.github.com>
2023-02-23 15:48:29 +0700

    update

--- pve/src/pvesource_set_allvmvars.sh
+++ pve/src/pvesource_set_allvmvars.sh
@@ -1343,7 +1343,7 @@ fi
 # Query for non-dhcp
 if [ -n "${IP}" ] && [ ! "$IP" = 'dhcp' ] || [ -n "${IP6}" ] && [ ! "$IP6" = 'dhcp' ]
 then
-  msg "Proxmox $ID_NUM_TMP numeric IDs must be greater than 100. $(if [ -n "${IP}" ] && [ $(echo "$IP" | awk -F'.' '{print $4}') >= '100' ]; then echo -e "We recommend the User uses the last octet or host section value of your ${HOSTNAME^} IPv4 address to set a valid ${ID_NUM_TYPE}. If this ${ID_NUM_TYPE} is not available then PVE will auto-generate a valid ${ID_NUM_TYPE} for the User to accept or reject."; fi)"
+  msg "Proxmox $ID_NUM_TMP numeric IDs must be greater than 100. $(if [ -n "${IP}" ] && [ "$(echo "$IP" | awk -F'.' '{print $4}')" -ge 100 ]; then echo -e "We recommend the User uses the last octet or host section value of your ${HOSTNAME^} IPv4 address to set a valid ${ID_NUM_TYPE}. If this ${ID_NUM_TYPE} is not available then PVE will auto-generate a valid ${ID_NUM_TYPE} for the User to accept or reject."; fi)"
   while true
   do
     read -p "Enter $ID_NUM_TYPE : " -e -i $ID_NUM_TMP ID_NUM
