675875d0e128caa80bba866be44a29337cb77be8
ahuacate <43461046+ahuacate@users.noreply.github.com>
2023-04-19 13:38:12 +0700

    update

--- pve/src/pvesource_ct_medialab_vaapipassthru.sh
+++ pve/src/pvesource_ct_medialab_vaapipassthru.sh
@@ -17,31 +17,34 @@ This script ONLY supports Proxmox hosts installed with a AMD/Intel CPU with inte
 
 In the next steps we will check if your PVE host hardware supports VA-API video encoding. If the check passes we will configure your CT for VA-API passthrough encoding/decoding/transcoding."
 
+# Check AMD APU hardware
+if [[ $(lscpu | grep "Vendor ID:\s*AuthenticAMD") ]]
+
 # Checking for PVE host VA-API support
 msg "Checking PVE host support for VA-API..."
 if [ $(ls -l /dev/dri | grep renderD128 > /dev/null; echo $?) = 0 ]
 then
-  # Installing VA-INFO
-  apt-get --only-upgrade install vainfo -y > /dev/null
-  apt-get install vainfo -y > /dev/null
-  if [[ $(dpkg -s vainfo 2> /dev/null) ]]
+  # Install VA-INFO
+  if [[ ! $(dpkg -s vainfo 2> /dev/null) ]]
   then
-    chmod 666 /dev/dri/renderD128 >/dev/null
-    # Creating rc.local script to set permissions for /dev/dri/renderD128
-    echo -e '#!/bin/sh -e\n/bin/chmod 666 /dev/dri/renderD128\nexit 0' > /etc/rc.local
-    chmod +x /etc/rc.local
-    bash /etc/rc.local
-    # Creating PVE host video device passthrough
-    DRM_VAR01=$(ls -l /dev/dri | grep renderD128 | awk '{print $5}' | sed "s/,//")
-    DRM_VAR02=$(ls -l /dev/dri | grep renderD128 | awk '{print $6}')
-    echo -e "lxc.cgroup2.devices.allow: c $DRM_VAR01:$DRM_VAR02 rwm\nlxc.cgroup2.devices.allow: c $DRM_VAR01:0 rwm\nlxc.mount.entry: /dev/dri/renderD128 dev/dri/renderD128 none bind,optional,create=file" >> /etc/pve/lxc/$CTID.conf
-    info "VA-API renderD128 is configured for '${HOSTNAME^}'."
-    echo
+    apt-get install vainfo -y > /dev/null
   else
-    warn "Problems installing 'vainfo' package (VA-API). Reason unknown. Try manually installing VA-API at later stage. Skipping this step."
-    sleep 2
-    echo
+    apt-get --only-upgrade install vainfo -y > /dev/null
   fi
+
+  # Configure VAAPI passthru
+  chmod 666 /dev/dri/renderD128 >/dev/null
+  # Creating rc.local script to set permissions for /dev/dri/renderD128
+  echo -e '#!/bin/sh -e\n/bin/chmod 666 /dev/dri/renderD128\nexit 0' > /etc/rc.local
+  chmod +x /etc/rc.local
+  bash /etc/rc.local
+
+  # Creating PVE host video device passthrough
+  DRM_VAR01=$(ls -l /dev/dri | grep renderD128 | awk '{print $5}' | sed "s/,//")
+  DRM_VAR02=$(ls -l /dev/dri | grep renderD128 | awk '{print $6}')
+  echo -e "lxc.cgroup2.devices.allow: c $DRM_VAR01:$DRM_VAR02 rwm\nlxc.cgroup2.devices.allow: c $DRM_VAR01:0 rwm\nlxc.mount.entry: /dev/dri/renderD128 dev/dri/renderD128 none bind,optional,create=file" >> /etc/pve/lxc/$CTID.conf
+  info "VA-API renderD128 is configured for '${HOSTNAME^}'."
+  echo
 else
   info "PVE host does not support VA-API renderD128. Skipping this step."
   sleep 2
