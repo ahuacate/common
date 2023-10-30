435d074f83ebf5b8f0905fcfeb73db3e0ca32d16
ahuacate <43461046+ahuacate@users.noreply.github.com>
2023-10-30 13:18:36 +0700

    Update ct uid/gid mapping script

--- pve/src/pvesource_ct_medialab_ctidmapping.sh
+++ pve/src/pvesource_ct_medialab_ctidmapping.sh
@@ -2,6 +2,7 @@
 # ----------------------------------------------------------------------------------
 # Filename:     pvesource_ct_medialab_ctidmapping.sh
 # Description:  Source script for CTID container mapping for medialab UID & GUID
+#               For Ubuntu CT UID/GIDs only
 # ----------------------------------------------------------------------------------
 
 #---- Source -----------------------------------------------------------------------
@@ -11,20 +12,38 @@
 #---- Other Files ------------------------------------------------------------------
 #---- Body -------------------------------------------------------------------------
 
-# Unprivileged container mapping
+#---- Unprivileged container mapping
+
+# CTID UID/GID maps
 msg "MediaLab unprivileged PVE CT mapping..."
-echo -e "lxc.idmap: u 0 100000 1605
-lxc.idmap: g 0 100000 100
-lxc.idmap: u 1605 1605 1
+echo -e "# Video grp map
+lxc.idmap: g 0 100000 44
+lxc.idmap: g 44 44 1
+lxc.idmap: g 45 100045 54
+# User grp map
 lxc.idmap: g 100 100 1
+lxc.idmap: g 101 100101 6
+# Render grp map
+lxc.idmap: g 108 103 1
+lxc.idmap: g 109 100109 65426
+# Ahuacate user(s) map
+lxc.idmap: u 0 100000 1605
+lxc.idmap: u 1605 1605 1
 lxc.idmap: u 1606 101606 63930
-lxc.idmap: g 101 100101 65435
-lxc.idmap: u 65604 65604 100
-lxc.idmap: g 65604 65604 100" >> /etc/pve/lxc/$CTID.conf
-grep -qxF 'root:65604:100' /etc/subuid || echo 'root:65604:100' >> /etc/subuid
-grep -qxF 'root:65604:100' /etc/subgid || echo 'root:65604:100' >> /etc/subgid
-grep -qxF 'root:100:1' /etc/subgid || echo 'root:100:1' >> /etc/subgid
-grep -qxF 'root:1605:1' /etc/subuid || echo 'root:1605:1' >> /etc/subuid
+# Ahuacate grp(s) map - medialab
+lxc.idmap: g 65605 65605 1" >> /etc/pve/lxc/$CTID.conf
+
+# /etc/subuid entries
+grep -qxF 'root:1605:1' /etc/subuid || echo 'root:1605:1' >> /etc/subuid  # User media
+
+# /etc/subgid entries
+grep -qxF 'root:44:1' /etc/subgid || echo 'root:44:1' >> /etc/subgid  # Grp video
+grep -qxF 'root:100:1' /etc/subgid || echo 'root:100:1' >> /etc/subgid  # Grp users
+grep -qxF 'root:103:1' /etc/subgid || echo 'root:103:1' >> /etc/subgid  # Grp render
+grep -qxF 'root:65605:1' /etc/subgid || echo 'root:65605:1' >> /etc/subgid  # Grp medialab
+
+# Display msg
 info "${CT_HOSTNAME_VAR^} CT 'unprivileged' UID mapping is set: ${YELLOW}media${NC}"
 info "${CT_HOSTNAME_VAR^} CT 'unprivileged' GUID mapping is set: ${YELLOW}medialab${NC}."
 echo
+#-----------------------------------------------------------------------------------
--- pve/src/pvesource_ct_privatelab_ctidmapping.sh
+++ pve/src/pvesource_ct_privatelab_ctidmapping.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # ----------------------------------------------------------------------------------
-# Filename:     pvesource_ct_homelab_ctidmapping.sh
-# Description:  Source script for CTID container mapping for homelab UID & GUID
+# Filename:     pvesource_ct_private_ctidmapping.sh
+# Description:  Source script for CTID container mapping for private UID & GUID
 #               For Ubuntu CT UID/GIDs only
 # ----------------------------------------------------------------------------------
 
@@ -27,20 +27,20 @@ lxc.idmap: g 101 100101 6
 lxc.idmap: g 108 103 1
 lxc.idmap: g 109 100109 65426
 # Ahuacate user(s) map
-lxc.idmap: u 0 100000 1606
-lxc.idmap: u 1606 1606 1
-lxc.idmap: u 1607 101607 63929
+lxc.idmap: u 0 100000 1607
+lxc.idmap: u 1607 1607 1
+lxc.idmap: u 1608 101608 63928
 # Ahuacate grp(s) map - homelab
-lxc.idmap: g 65606 65606 1" >> /etc/pve/lxc/$CTID.conf
+lxc.idmap: g 65607 65607 1" >> /etc/pve/lxc/$CTID.conf
 
 # /etc/subuid entries
-grep -qxF 'root:1606:1' /etc/subuid || echo 'root:1606:1' >> /etc/subuid  # User home
+grep -qxF 'root:1607:1' /etc/subuid || echo 'root:1607:1' >> /etc/subuid  # User private
 
 # /etc/subgid entries
 grep -qxF 'root:44:1' /etc/subgid || echo 'root:44:1' >> /etc/subgid  # Grp video
 grep -qxF 'root:100:1' /etc/subgid || echo 'root:100:1' >> /etc/subgid  # Grp users
 grep -qxF 'root:103:1' /etc/subgid || echo 'root:103:1' >> /etc/subgid  # Grp render
-grep -qxF 'root:65606:1' /etc/subgid || echo 'root:65606:1' >> /etc/subgid  # Grp homelab
+grep -qxF 'root:65607:1' /etc/subgid || echo 'root:65607:1' >> /etc/subgid  # Grp privatelab
 
 # Display msg
 info "${CT_HOSTNAME_VAR^} CT 'unprivileged' UID mapping is set: ${YELLOW}home${NC}"
