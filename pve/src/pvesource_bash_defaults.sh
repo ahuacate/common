9118956b0dd4dbcba8e5e4c2562a9d5eb58e4b8d
ahuacate <43461046+ahuacate@users.noreply.github.com>
2023-11-02 12:36:53 +0700

    update pve version checker

--- pve/src/pvesource_bash_defaults.sh
+++ pve/src/pvesource_bash_defaults.sh
@@ -91,11 +91,72 @@ function load_module() {
 }
 # Installer cleanup
 function installer_cleanup() {
-rm -R $REPO_TEMP/$GIT_REPO &> /dev/null
-if [ -f "$REPO_TEMP/${GIT_REPO}.tar.gz" ]
-then
-  rm $REPO_TEMP/${GIT_REPO}.tar.gz > /dev/null
-fi
+  rm -R $REPO_TEMP/$GIT_REPO &> /dev/null
+  if [ -f "$REPO_TEMP/${GIT_REPO}.tar.gz" ];then
+    rm $REPO_TEMP/${GIT_REPO}.tar.gz > /dev/null
+  fi
+}
+
+#--- Check Proxmox and kernel version
+# Get PVE version status
+function pve_version_status() {
+    # Function retrieves Proxmox and Kernel versions
+    # and informs the user if a upgrade is recommended.
+    # This func does not require prerequisite 'pvesource_bash_defaults.sh
+    # This func is used in 'pve_repo_loader' by default.
+
+    local pve_vers=$(pveversion -v | grep 'proxmox-ve:*' | awk '{ print $2 }' | sed 's/\..*$//')
+    local kernel_vers=$(uname -r | cut -d'.' -f1,2)
+    local pve7_kernel_min=5.19  # Recommended min Kernel version
+    local pve8_kernel_min=6.2  # Recommended min Kernel version
+
+    if [[ ! $(dpkg -s boxes 2> /dev/null) ]]; then apt-get install -y boxes > /dev/null; fi  # Install boxes if missing
+
+    # Run PVE check
+    if [ "$pve_vers" -lt 7 ]; then
+        # Display msg for less than PVE7
+        local msg_body1="#### URGENT: PROXMOX UPGRADE REQUIRED ####\n\nYour Proxmox version (v$pve_vers) is no longer supported. It is strongly recommended to update Proxmox to the latest version before proceeding with our scripts.\n\nIf you choose to continue using the current version, we cannot guarantee the stability or functionality of our scripts."
+        echo -e "\e[33m$(echo -e "$msg_body1" | fmt -s -w 80 | boxes -n utf8 -d stone -p a1l3 -s 84)\e[0m"  # Display msg
+        echo
+        sleep 0.5
+    elif [ "$pve_vers" = 7 ] && [ "$(echo "$kernel_vers" | awk -F. '{print $1$2}')" -lt "$(echo "$pve7_kernel_min" | sed 's/\.//g')" ]; then
+        # Display msg for PVE7 wrong kernel
+        msg_body2="Your Proxmox (v$pve_vers) installation is supported, but it requires an update to the kernel. To enable LXC to use iGPU rendering in Medialab applications, you must upgrade your kernel to at least version v$pve7_kernel_min.\n\nHere are the CLI commands to perform the kernel upgrade:\n\n  -- apt update\n  -- apt install pve-kernel-$pve7_kernel_min\nAlternatively, we recommend updating Proxmox to the latest version for the best overall performance and compatibility."
+        echo -e "\e[33m$(echo -e "$msg_body2" | fmt -s -w 80 | boxes -n utf8 -d stone -p a1l3 -s 84)\e[0m"  # Display msg
+        echo
+        sleep 0.5
+    elif [ "$pve_vers" = 8 ] && [ "$(echo "$kernel_vers" | awk -F. '{print $1$2}')" -lt "$(echo "$pve8_kernel_min" | sed 's/\.//g')" ]; then
+        # Display msg for PVE8 wrong kernel
+        msg_body3="Your Proxmox (v$pve_vers) installation is supported, but it requires an update to the kernel. To enable LXC to use iGPU rendering in Medialab applications, you must upgrade your kernel to at least version v$pve8_kernel_min.\n\nHere are the CLI commands to perform the kernel upgrade:\n\n  -- apt update\n  -- apt install pve-kernel-$pve8_kernel_min\nAlternatively, we recommend updating Proxmox to the latest version for the best overall performance and compatibility."
+        echo -e "\e[33m$(echo -e "$msg_body3" | fmt -s -w 80 | boxes -n utf8 -d stone -p a1l3 -s 84)\e[0m"  # Display msg
+        echo
+        sleep 0.5
+    else
+        return  # Return to parent script
+    fi
+
+    # User read confirmation
+    while true; do
+        read -p "I have reviewed this information and understand. Proceed with this install [y/n]?: " -n 1 -r YN
+        echo
+        case $YN in
+            [Yy]*)
+            msg "You have been warned. Proceeding..."
+            echo
+            break
+            ;;
+            [Nn]*)
+            info "The User has chosen to not to proceed. Smart decision..."
+            echo
+            sleep 1
+            return 0
+            ;;
+            *)
+            warn "Error! Entry must be 'y' or 'n'. Try again..."
+            echo
+            ;;
+        esac
+    done
 }
 
 #---- User and Password Functions
--- pve/src/pvesource_ct_medialab_vaapipassthru.sh
+++ pve/src/pvesource_ct_medialab_vaapipassthru.sh
@@ -19,32 +19,31 @@ In the next steps we will check if your PVE host hardware supports VA-API video 
 
 # Checking for PVE host VA-API support
 msg "Checking PVE host support for VA-API..."
-if [ $(ls -l /dev/dri | grep renderD128 > /dev/null; echo $?) = 0 ]
-then
-  # Install VA-INFO
-  if [[ ! $(dpkg -s vainfo 2> /dev/null) ]]
-  then
-    apt-get install vainfo -y > /dev/null
-  else
-    apt-get --only-upgrade install vainfo -y > /dev/null
-  fi
+if [ $(ls -l /dev/dri | grep renderD128 > /dev/null; echo $?) = 0 ]; then
+    # Install VA-INFO
+    if [[ ! $(dpkg -s vainfo 2> /dev/null) ]]; then
+        apt-get install vainfo -y > /dev/null
+    else
+        apt-get --only-upgrade install vainfo -y > /dev/null
+    fi
 
-  # Configure VA-API passthru
-  chmod 666 /dev/dri/renderD128 >/dev/null
-  # Creating rc.local script to set permissions for /dev/dri/renderD128
-  echo -e '#!/bin/sh -e\n/bin/chmod 666 /dev/dri/renderD128\nexit 0' > /etc/rc.local
-  chmod +x /etc/rc.local
-  bash /etc/rc.local
+    # Configure VA-API passthru
+    chmod 666 /dev/dri/renderD128 >/dev/null
+    # Creating rc.local script to set permissions for /dev/dri/renderD128
+    echo -e '#!/bin/sh -e\n/bin/chmod 666 /dev/dri/renderD128\nexit 0' > /etc/rc.local
+    chmod +x /etc/rc.local
+    bash /etc/rc.local
 
-  # Creating PVE host video device passthrough
-  DRM_VAR01=$(ls -l /dev/dri | grep renderD128 | awk '{print $5}' | sed "s/,//")
-  DRM_VAR02=$(ls -l /dev/dri | grep renderD128 | awk '{print $6}')
-  echo -e "lxc.cgroup2.devices.allow: c $DRM_VAR01:$DRM_VAR02 rwm\nlxc.cgroup2.devices.allow: c $DRM_VAR01:0 rwm\nlxc.mount.entry: /dev/dri/renderD128 dev/dri/renderD128 none bind,optional,create=file\nlxc.mount.entry: /dev/dri dev/dri none bind,optional,create=dir" >> /etc/pve/lxc/$CTID.conf
-  info "VA-API renderD128 is configured for '${HOSTNAME^}'."
-  echo
+    # Creating PVE host video device passthrough
+    DRM_VAR01=$(ls -l /dev/dri | grep renderD128 | awk '{print $5}' | sed "s/,//")
+    DRM_VAR02=$(ls -l /dev/dri | grep renderD128 | awk '{print $6}')
+    echo -e "lxc.cgroup2.devices.allow: c $DRM_VAR01:$DRM_VAR02 rwm\nlxc.cgroup2.devices.allow: c $DRM_VAR01:0 rwm\nlxc.mount.entry: /dev/dri/renderD128 dev/dri/renderD128 none bind,optional,create=file\nlxc.mount.entry: /dev/dri dev/dri none bind,optional,create=dir" >> /etc/pve/lxc/$CTID.conf
+    info "LXC to iGPU render (VA-API) is configured for '${HOSTNAME^}'."
+    echo
 else
-  info "PVE host does not support VA-API renderD128. Skipping this step."
-  sleep 2
-  echo
+    info "Your PVE host currently does not support LXC to iGPU render (VA-API). To fix this issue run our 'PVE Host Toolbox' easy script and select menu item 'PVE Intel Amd iGPU LXC support' to setup LXC access to your CPUs integrated graphics processor. You will need to reinstall this LXC.
+    LXC to iGPU render (VA-API) is NOT configured for '${HOSTNAME^}'. Skipping this step."
+    sleep 3
+    echo
 fi
 #-----------------------------------------------------------------------------------
