6e72418c3bc8a559cb648234ee7a55b6db8b188e
ahuacate <43461046+ahuacate@users.noreply.github.com>
2024-06-04 15:57:13 +0700

    update

--- bash/src/basic_bash_sw.sh
+++ bash/src/basic_bash_sw.sh
@@ -28,6 +28,11 @@ if [[ ! $(dpkg -s curl 2> /dev/null) ]]; then
   apt-get install curl -y
 fi
 
+#--- Install jq
+if [[ ! $(dpkg -s jq 2> /dev/null) ]]; then
+  apt-get install jq -y
+fi
+
 #--- Check for linux ascii boxes
 if command -v boxes > /dev/null; then
   current_ver=$(boxes -v | awk '{print $3}')  # Get the current version of boxes
@@ -35,9 +40,12 @@ else
   current_ver=0 # Set version to null
 fi
 
-# Get version status
-latest_ver_tag=$(curl -s https://api.github.com/repos/ascii-boxes/boxes/releases/latest | grep -oP '"tag_name": "\K(.*?)(?=")')  # Get the latest version from GitHub releases
-latest_ver=$(echo "$latest_ver_tag" | sed 's/^v//')  # Remove the leading 'v' from the version number
+# Fetch the latest release JSON from GitHub API
+response=$(curl -s https://api.github.com/repos/ascii-boxes/boxes/releases/latest)
+
+# Extract the latest version using jq
+latest_ver_tag=$(echo "$response" | jq -r '.tag_name')
+latest_ver=$(echo "$response" | jq -r '.tag_name' | sed 's/v//')
 
 # Compare Ascii Box versions
 if [[ "$current_ver" < "$latest_ver" ]]; then
@@ -56,7 +64,7 @@ if [[ "$current_ver" < "$latest_ver" ]]; then
   fi
 
   # Install prerequisites 
-  apt-get install -y build-essential diffutils flex bison libunistring-dev libpcre2-dev libcmocka-dev git vim-common 2> /dev/null
+  apt-get install -y build-essential diffutils flex bison libunistring-dev libpcre2-dev libcmocka-dev libncurses-dev git terminfo vim-common 2> /dev/null
 
   # Download the latest version from GitHub releases
   wget -P "/tmp" "https://github.com/ascii-boxes/boxes/archive/$latest_ver_tag.tar.gz"
